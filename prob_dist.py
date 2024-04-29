4591e4d8c0f6adb7b7b23801ca7525535b30e16d
aryansubramanian4 <asubramanian4@babson.edu>
2024-04-29 17:49:15 -0400

    code so far

--- prob_dist.py
+++ prob_dist.py
@@ -63,18 +63,18 @@ def dist_parameters(distribution_type):
     if distribution_type == 'continuous':
         user_choice = continuous_dist()
 
-        if user_choice == 1:
+        if user_choice == 1: #Beta
             alpha = float(input("Please enter the alpha parameter: "))
             beta = float(input("Please enter the beta parameter: "))
             return x, user_choice, alpha, beta
-        elif user_choice == 2:
+        elif user_choice == 2: #Gamma
             shape = float(input("Please enter the shape parameter: "))
             scale = float(input("Please enter the scale parameter: "))
             return x, user_choice, shape, scale
-        elif user_choice == 3:
+        elif user_choice == 3: #Exponential
             lambda_1 = float(input("Please enter the lambda parameter: "))
             return x, user_choice, lambda_1
-        elif user_choice == 4:
+        elif user_choice == 4: #Normal
             mean = float(input("Please enter the mean parameter: "))
             std_dev = float(input("Please enter the standard deviation parameter: "))
             return x, user_choice, mean, std_dev
@@ -82,19 +82,19 @@ def dist_parameters(distribution_type):
     elif distribution_type == 'discrete':
         user_choice = discrete_dist()
 
-        if user_choice == 1:
+        if user_choice == 1: #Binomial
             n = int(input("Please enter the number of trials (n): "))
             p = float(input("Please enter the probability of success for each trial: "))
             return x, user_choice, n, p
-        elif user_choice == 2:
+        elif user_choice == 2: #Geometric
             p = float(input("Please enter the probability of success for each trial: "))
             return x, user_choice, p
-        elif user_choice == 3:
+        elif user_choice == 3: #Hypergeometric
             cap_n = int(input("Please enter the population size (N): "))
             r = int(input("Please enter the number in the population with given characteristic (r): "))
             n = int(input("Please enter the sample size (n): "))
             return x, user_choice, cap_n, r, n
-        elif user_choice == 4:
+        elif user_choice == 4: #Poisson
             lambda_2 = float(input("Please enter the lambda parameter: "))
             return x, user_choice, lambda_2
 
@@ -112,41 +112,41 @@ def calculate_prob(distribution_type, choice, x, *parameters):
 
     if distribution_type == 'continuous':
         
-        if choice == 1:
+        if choice == 1: #Beta
             alpha, beta = parameters
             results['P(X < x)'] = stats.beta.cdf(x, alpha, beta)
             results['P(X > x)'] = 1 - stats.beta.cdf(x, alpha, beta)
-        elif choice == 2:
+        elif choice == 2: #Gamma
             shape, scale = parameters
             results['P(X < x)'] = stats.gamma.cdf(x, shape, scale)
             results['P(X > x)'] = 1 - stats.gamma.cdf(x, shape, scale)
-        elif choice == 3:
+        elif choice == 3: #Exponential
             lambda_1, = parameters
             results['P(X < x)'] = stats.expon.cdf(x, lambda_1)
             results['P(X > x)'] = 1 - stats.expon.cdf(x, lambda_1)
-        elif choice == 4:
+        elif choice == 4: #Normal
             mean, std_dev = parameters
             results['P(X < x)'] = stats.norm.cdf(x, mean, std_dev)
             results['P(X > x)'] = 1 - stats.norm.cdf(x, mean, std_dev)
 
     elif distribution_type == 'discrete':
         
-        if choice == 1:
+        if choice == 1: #Binomial
             n, p = parameters
             results['P(X = x)'] = stats.binom.pmf(x, n, p)
             results['P(X > x)'] = 1 - stats.binom.cdf(x, n, p)
             results['P(X < x)'] = stats.binom.cdf(x - 1, n, p)
-        elif choice == 2:
+        elif choice == 2: #Geometric
             p, = parameters
             results['P(X = x)'] = stats.geom.pmf(x, p)
             results['P(X > x)'] = 1 - stats.geom.cdf(x, p)
             results['P(X < x)'] = stats.geom.cdf(x - 1, p)
-        elif choice == 3:
+        elif choice == 3: #Hypergeometric
             cap_n, r, n = parameters
             results['P(X = x)'] = stats.hypergeom.pmf(x, cap_n, r, n)
             results['P(X > x)'] = 1 - stats.hypergeom.cdf(x, cap_n, r, n)
             results['P(X < x)'] = stats.hypergeom.cdf(x - 1, cap_n, r, n)
-        elif choice == 4:
+        elif choice == 4: #Poisson
             lambda_2, = parameters
             results['P(X = x)'] = stats.poisson.pmf(x, lambda_2)
             results['P(X > x)'] = 1 - stats.poisson.cdf(x, lambda_2)
